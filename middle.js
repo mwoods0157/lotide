294416ce675255a43edebdd187836999555d79fd
matthew woods <matthew.woods0157@gmail.com>
2021-11-23 22:09:54 +0000

    This is the middle addition to the lotide repo

---
+++ middle.js
@@ -0,0 +1,36 @@
+//function needs to take an array
+//For arrays with 1 || 2 elements return an empty array
+//For arrays with odd number of elements return a single middle element
+//For arrays with even number of elements return a double array
+
+const middle = function(array) {
+    let middleValue = [];
+    //test whether the values are 1||2
+    if (array.length === 1 || array.length === 2) {
+        return middleValue;
+    //Create the two conditions
+    } else if (array.length % 2 === 1) {
+        middleValue.push(array[(array.length / 2) - 0.5])
+    } else if (array.length % 2 === 0) {
+        middleValue.push(array[(array.length / 2) - 1]);
+        middleValue.push(array[(array.length / 2)]);
+    }
+    return middleValue;
+}
+
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log(`✅ ✅ ✅Assertion Passed: ${actual} === ${expected}`);
+    } else {
+        console.log(`🛑 🛑 🛑Assertion Failed: ${actual} !== ${expected}`);
+    }
+}
+
+const eqArrays = function(actual, expected) {
+    for (let i = 0; 0 < actual.length; i++) {
+        if (actual[i] !== expected[i]) {
+            return false;
+        }
+    }
+    return true;
+}
