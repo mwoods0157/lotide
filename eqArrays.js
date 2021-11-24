48321cbf39b96052a397d0aa540adf25ca982303
matthew woods <matthew.woods0157@gmail.com>
2021-11-24 23:26:47 +0000

    New eqArrays.js value, this one works

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,15 @@
-//Needs to take 2 arrays and checks whether they are a prefect match
-//Type specific (=== not ==)
-
-const eqArrays = function(actual, expected) {
-    for (let i = 0; 0 < actual.length; i++) {
-        if (actual[i] !== expected[i]) {
-            return false;
+const eqArrays = function(array1, array2) {
+    if (array1.length !== array2.length) {
+        return false;
+    } else {
+        for (let i = 0; i < array1.length; i++) {
+            if (array1[i] !== array2[i]) {
+                return false;
+            }
         }
     }
     return true;
 }
 
-console.log(eqArrays([1, 2],[1, 2]));
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
