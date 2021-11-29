655305ddea35e53f8036b4d78e9f06d9cd74a48f
matthew woods <matthew.woods0157@gmail.com>
2021-11-29 15:18:38 +0000

    Refactored assertArraysEqual file to add to the lotide repo

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,18 @@
-const eqArrays = function(actual, expected) {
-    for (let i = 0; 0 < actual.length; i++) {
-        if (actual[i] !== expected[i]) {
-            return false;
+const eqArrays = require('./eqArrays');
+
+/* const eqArrays = function(array1, array2) {
+    if (array1.length !== array2.length) {
+        return false;
+    } else {
+        for (let i = 0; i < array1.length; i++) {
+            if (array1[i] !== array2[i]) {
+                return false;
+            }
         }
     }
     return true;
-}
+} */
+
 //Takes two arrays: actual and expected
 //Returns console.log with appropriate message.Template literal string
 //Should also make sure of the eqArray() function
@@ -14,3 +21,6 @@ const eqArrays = function(actual, expected) {
 const assertArraysEqual = function(actual, expected) {
     return (eqArrays(actual, expected) ? console.log(`✅ ✅ ✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑 🛑 🛑Assertion Failed: ${actual} !== ${expected}`));
 }
+
+//console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+module.exports = assertArraysEqual;
