9ac2fe804c8d4763d42dba9e550c58fab8b9fa90
matthew woods <matthew.woods0157@gmail.com>
2021-11-29 19:34:34 +0000

    Adding tailTest file to the lotide repo

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,20 @@
 const tail = require('../tail');
-const assertArraysEqual = require('../assertArraysEqual');
+//const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual(tail([2, 3, 4], [3, 4]));
-assertArraysEqual(tail([2, 3, 4], [2, 4]));
+describe('#tail', () => {
+    it('#1 returns "[3, 4]" if input is "[2, 3, 4]"', () => {
+        assert.deepEqual(tail([2, 3, 4]), [3, 4]);
+    })
+
+    it('#2 returns "[1, 2, 3]" if the input is "[0, 1, 2, 3]"', () => {
+        assert.deepEqual(tail([0, 1, 2, 3,]), [1, 2, 3]);
+    })
+
+    it('#3 returns "[1, 2, 4]" if the input is "[6, 1, 2, 4]"', () => {
+        assert.deepEqual(tail([9, 1, 2, 4]), [1, 2, 4]);
+    })
+})
+
+//assertArraysEqual(tail([2, 3, 4], [3, 4]));
+//assertArraysEqual(tail([2, 3, 4], [2, 4]));
