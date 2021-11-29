4172a7bfdf72a8dbd84d066d1f0123d03f6cc747
matthew woods <matthew.woods0157@gmail.com>
2021-11-29 15:01:56 +0000

    Adding the refactored eqArrays.js file to the lotide repo

--- eqArrays.js
+++ eqArrays.js
@@ -13,3 +13,6 @@ const eqArrays = function(array1, array2) {
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3]));
 console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+
+console.log(eqArrays([1], [2]));
+module.exports = eqArrays;
