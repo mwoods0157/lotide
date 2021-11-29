6850e3e10947a6825359169a1c70bddc221fdbd4
matthew woods <matthew.woods0157@gmail.com>
2021-11-29 19:43:18 +0000

    Adding middleTest.js to the lotide repo

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,24 @@
-const assertArraysEqual = require('../assertArraysEqual');
+//const assertArraysEqual = require('../assertArraysEqual');
 //const eqArrays = require('./eqArrays');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])); //Should return true
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [4])); //Should return false
+describe('#middle', () => {
+    it('#1 returns "[3]" given an input of "[1, 2, 3, 4, 5]"', () => {
+        assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+    })
+
+    it('#2 returns "[3, 4]" given an input of "[1, 2, 3, 4, 5, 6]"', () => {
+        assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+    })
+
+    //This should return an error because the middle value of the array is 3. Testing to see if the function is jsut passing true for everything
+    it('#3 returns "[4]" give an input of "[1, 2, 3, 4, 5]"', () => {
+        assert.deepEqual(middle([1, 2, 3, 4, 5]), [4]);
+    })
+})
+
+//console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])); //Should return true
+//console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [4])); //Should return false
 
 
